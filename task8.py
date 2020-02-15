9275ece68ff04ca331582744d70a94897516c88f
shmatov-nikolai <grekkg@gmail.com>
2020-02-15 16:45:45 +0600

    first commit

---
+++ task8.py
@@ -0,0 +1,10 @@
+'''
+Given two integers A and B (A ≤ B).
+ Print all numbers from A to B inclusively.
+'''
+
+def range_dif_num(a,b):
+   for i in range(a,b+1):
+       print (i)
+
+range_dif_num(5,25)
